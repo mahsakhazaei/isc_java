efea8545f1ec361e7afb0783e013c00f8f0235b0
mahsakhazaei <63585423+mahsakhazaei@users.noreply.github.com>
2024-11-15 18:47:55 +0330

    Delete isc.HW5 directory

--- isc.HW5/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module scores {
-	requires java.logging;
-}
--- isc.HW5/shop/main.java
+++
@@ -1,8 +0,0 @@
-package HW5.shop;
-
-public class main {
-	public main(String[] args) {
-		new Service();
-		Service.PrintSaleProducts();
-}
-}
